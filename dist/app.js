7396a29ccc9bfa83167da3af94cd4193c8779f75
David Arinze <fivelanes72@gmail.com>
2024-07-08 10:35:25 +0100

    Initial Setup: Installed dependencies and configured the typescript config file

---
+++ dist/app.js
@@ -0,0 +1,38 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+/**------------------------ */
+const index_1 = __importDefault(require("./routes/index"));
+const postRoutes_1 = __importDefault(require("./routes/postRoutes"));
+const database_1 = __importDefault(require("./config/database"));
+// Initialize database
+(0, database_1.default)();
+//Initialize express
+const app = (0, express_1.default)();
+//Middleware Setup
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, morgan_1.default)('dev'));
+//Routes
+app.use('/', index_1.default);
+app.use('/api/posts', postRoutes_1.default);
+// Middleware to handle 404 errors
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error); // Pass the error to the next middleware
+});
+// Error-handling middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message || 'Internal Server Error',
+        },
+    });
+});
+exports.default = app;
---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import { Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
+/**------------------------ */
+import indexRoute from './routes/index';
+import postRoutes from './routes/postRoutes';
+import main from './config/database';
+
+// Initialize database
+main();
+
+//Initialize express
+const app = express();
+
+//Middleware Setup
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+app.use(morgan('dev'));
+
+
+//Routes
+app.use('/', indexRoute);
+app.use('/api/posts', postRoutes);
+
+// Middleware to handle 404 errors
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const error: any = new Error('Not Found');
+    error.status = 404;
+    next(error); // Pass the error to the next middleware
+});
+  
+  // Error-handling middleware
+  app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message || 'Internal Server Error',
+      },
+    });
+});
+
+export default app;
